e5b18ddf048d46b400959ae4aa54eb9614d7977f
zhaoweihan2015 <727095319@qq.com>
2017-11-06 09:45:38 +0800

    抽离公共页面

--- index.php
+++ index.php
@@ -64,33 +64,7 @@
     <!-- 主体 -->
     <div class="main">
         <div class="container-fluid">
-            <!-- 头部 -->
-            <div class="header">
-                <nav class="navbar navbar-custom">
-                    <div class="navbar-header">
-                        <a href="javascript:;" class="navbar-brand">
-                            <i class="fa fa-navicon"></i>
-                        </a>
-                    </div>
-                    <ul class="nav navbar-nav navbar-right">
-                        <li>
-                            <a href="./settings.html">
-                                <i class="fa fa-user"></i> 个人中心
-                            </a>
-                        </li>
-                        <li>
-                            <a href="javascript:;" class="logout">
-                                <i class="fa fa-sign-out"></i> 退出
-                            </a>
-                        </li>
-                        <li>
-                            <a href="javascript:;">
-                                <i class="fa fa-tasks"></i>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
+            <?php include './common/header.html'?>
             <!-- 个人资料 -->
             <div class="body dashboard">
                 <div class="row survey">
@@ -129,8 +103,7 @@
             </div>
         </div>
     </div>
-    <script src="/public/js/require.js"></script>
-    <script src="/public/js/config.js"></script>
+    <?php include './common/script.html'?>
     <script>
         require(['src/index']);
     </script>
