a1cdf7caa36460414e1ce2f45b16a39d0e8860d0
zhaoweihan2015 <727095319@qq.com>
2017-11-06 09:51:09 +0800

    抽离公共页面2

--- index.php
+++ index.php
@@ -4,63 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <title>乐淘 - 后台管理</title>
-    <link rel="stylesheet" href="public/assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="public/assets/font-awesome/css/font-awesome.css">
-    <link rel="stylesheet" href="public/assets/nprogress/nprogress.css">
-    <link rel="stylesheet" href="public/css/index.css">
+    <?php include './common/style.html'?>    
 </head>
 
 <body>
-
-    <!-- 侧边栏 -->
-    <div class="aside">
-        <!-- 个人资料 -->
-        <div class="profile">
-            <!-- 头像 -->
-            <div class="avatar img-circle">
-                <img src="./uploads/avatar.jpg">
-            </div>
-            <h4>布头儿</h4>
-        </div>
-        <!-- 导航菜单 -->
-        <div class="navs">
-            <ul class="list-unstyled">
-                <li>
-                    <a href="./index.html" class="active">
-                        <i class="fa fa-dashboard"></i> 仪表盘
-                    </a>
-                </li>
-                <li>
-                    <a href="./user_list.html">
-                        <i class="fa fa-group"></i> 用户管理
-                    </a>
-                </li>
-                <li>
-                    <a href="javascript:;">
-                        <i class="fa fa-indent"></i> 分类管理
-                        <i class="arrow fa fa-angle-right"></i>
-                    </a>
-                    <ul class="list-unstyled">
-                        <li>
-                            <a href="./category.html">
-                                 一级分类
-                            </a>
-                        </li>
-                        <li>
-                            <a href="./brand.html">
-                                二级分类
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="./goods_list.html">
-                        <i class="fa fa-cubes pull-left"></i> 商品管理
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </div>
+    <?php include './common/aside.html' ?>
     <!-- 主体 -->
     <div class="main">
         <div class="container-fluid">
